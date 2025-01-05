ee961c3c638bac853312329f8c25a588f8486b7f
Tino Didriksen <mail@tinodidriksen.com>
2025-01-05 12:56:30 +0100

    2025-01-05 11:56:29

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 D=`date -u '+%Y-%m-%d %H:%M:%S'`
-cp -av /cygdrive/c/Documents/Google\ Drive/Music/iTunes/*.xml ./
+cp -av /cygdrive/c/Documents/OneDrive/Media/Music/iTunes/*.xml ./
 perl -pe 's/\r\n/\n/g;' -i 'iTunes Music Library.xml'
 chmod 0664 *.xml
 rm -fv *.bak
